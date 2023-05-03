6c5b7292f699b318a3c330adb6dd0b77bd176398
Abdullah Othman <ur3fa7@inf.elte.hu>
2023-05-03 02:25:03 +0200

    BS-35: Add game timer class

--- models/Panels/DescriptionPanel.py
+++ models/Panels/DescriptionPanel.py
@@ -1,4 +1,10 @@
+import pygame
 from models.Panels.Panel import Panel
 class DescriptionPanel(Panel):
     def __init__(self,x,y,width, height):
         super().__init__(x,y,width,height)
+
+    def displayTime(self,screen,time,text_position):
+        font = pygame.font.Font(None,20)
+        text_surface = font.render(time, True, (255, 255, 255))
+        screen.blit(text_surface,text_position)
